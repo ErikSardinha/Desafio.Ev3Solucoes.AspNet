fdf547da73179bdc3f177d4f917c24e8194c690c
Erik Santos Sardinha <erik.sardinha@hotmail.com>
2018-05-04 18:51:17 -0300

    Pastas

---
+++ Ev3Solucoes/Ev3Solucoes/Dados/Evento.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ev3Solucoes.Dados
+{
+    public class Evento
+    {
+        public static bool Cadastrar(Models.Evento evento)
+        {
+            try
+            {
+                using (var conexao = new Models.BancoDeDadosEv3Entities())//Cria uma conexao com o Banco momentanea, até a pagina ser fechada
+                {
+                    conexao.Evento.Add(evento);// Adiciona um evento ao banco de dados
+                    conexao.SaveChanges();// Salva as alterações;
+                    return true;
+                }
+            }
+            catch (Exception Erro)
+            {
+                return false;
+            }
+        }
+        public static IEnumerable<Models.Evento> Exibir(){
+            try
+            {
+                using (var conexao = new Models.BancoDeDadosEv3Entities())//Conexão com o BD momentanea
+                {
+                    return conexao.Evento.ToList();//Retorna em formato de lista os elementos de Evento;
+                }
+            }
+            catch {
+                return new List<Models.Evento>();
+            }
+
+        }
+    }
+}
